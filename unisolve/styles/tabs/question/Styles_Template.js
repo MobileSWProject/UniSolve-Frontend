f73fa6cca57b050ed40f8dc9a432b9fa11bd14bd
ICE0208 <doky3307@naver.com>
2024-09-14 01:52:13 +0900

    docs: 스타일 템플릿 추가

---
+++ unisolve/styles/tabs/question/Styles_Template.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+// import { mainColor } from "../../../constants/Colors";
+
+const styles = StyleSheet.create({
+  //   container: {
+  //     flex: 1,
+  //     justifyContent: "center",
+  //     alignItems: "center",
+  //     gap: "20px",
+  //   },
+  //   mainView: {
+  //     color: mainColor,
+  //   },
+  //   mainText: {
+  //     fontSize: "20px",
+  //     fontWeight: "bold",
+  //   },
+});
+
+export default styles;
