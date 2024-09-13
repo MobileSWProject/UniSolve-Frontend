0711b41e297f942c1cf9cd7ac29cebb0825e037e
ICE0208 <doky3307@naver.com>
2024-09-14 01:48:45 +0900

    docs: 컴포넌트 템플릿 추가

---
+++ unisolve/components/tabs/community/Component_Template.jsx
@@ -0,0 +1,13 @@
+// import { useState } from "react";
+
+export default function ComponentName({ prop1, prop2, ...restProps }) {
+  // 로직들
+  // const [state, setState] = useState("");
+
+  // return jsx
+  return (
+    <View>
+      <Text>Component Template Text</Text>
+    </View>
+  );
+}
