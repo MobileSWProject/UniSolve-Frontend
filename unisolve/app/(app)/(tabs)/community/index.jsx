bd54b72377a6df8b25fe91e95133dd4f9505b743
Developer-KTH <cos4300@naver.com>
2024-09-29 21:12:18 +0900

    feat: axios Test

--- unisolve/app/(app)/(tabs)/community/index.jsx
+++ unisolve/app/(app)/(tabs)/community/index.jsx
@@ -1,6 +1,8 @@
 import { FlatList } from "react-native";
-import { useState } from "react";
+import { useState, useCallback } from "react";
+import { useFocusEffect } from '@react-navigation/native';
 import List from "../../../../components/tabs/List/List"
+import _axios from '../../../../api';
 
 export default function Community() {
   // 백엔드에서 Array[Object] 형태로 response(Value에서 Type은 상관 없음)
@@ -8,6 +10,16 @@ export default function Community() {
   const [communitys, setCommunitys] = useState([
     { id: 1324, questioner:'닉네임', title: 'React Native 질문', description: 'React Native에서', timestamp: '2024.8.31 20:18', reply: 0 },
   ]);
+  useFocusEffect(
+    useCallback(() => {
+      _axios.get('/community').then(response => {
+        setCommunitys(response.data || []);
+      })
+      .catch(error => {
+        setCommunitys([]);
+      })
+    }, [])
+  );
   return (
     <FlatList
       data={communitys}
