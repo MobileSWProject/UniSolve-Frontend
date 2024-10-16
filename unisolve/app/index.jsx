e0eb083a6938846b1e62d82ed9d8c4c7ba5f4e01
ICE0208 <doky3307@naver.com>
2024-10-16 22:45:38 +0900

    chore: react-native-syntax-highlighter 경고 메세지 제거

--- unisolve/app/index.jsx
+++ unisolve/app/index.jsx
@@ -1,3 +1,16 @@
+// react-native-syntax-highlighter의 defaultProps 경고 메세지 앱에서 안나오게
+import { LogBox } from "react-native";
+LogBox.ignoreLogs(["defaultProps"]);
+
+// react-native-syntax-highlighter의 defaultProps 에러 메세지 로그에서 안나오게
+const originalError = console.error;
+console.error = (message, ...args) => {
+  if (typeof message === "string" && message.includes("defaultProps")) {
+    return;
+  }
+  originalError(message, ...args);
+};
+
 import React, { useEffect } from "react";
 import { View, Text, StyleSheet, Image } from "react-native"; // Image 추가
 import { useRouter } from "expo-router";
@@ -16,9 +29,9 @@ export default function Home() {
     <View style={styles.container}>
       <Text style={styles.text}>UniSolve</Text>
       <Image
-          source={require('../assets/logo.jpg')} 
-          style={styles.logo}
-        />
+        source={require("../assets/logo.jpg")}
+        style={styles.logo}
+      />
       <Text style={styles.text}>환영합니다!</Text>
       <Text>곧 이동합니다.</Text>
     </View>
@@ -35,9 +48,10 @@ const styles = StyleSheet.create({
     fontSize: 20,
     marginBottom: 20,
   },
-  logo: { //로고 이미지
-    width: 250, 
+  logo: {
+    //로고 이미지
+    width: 250,
     height: 250,
     marginBottom: 20,
-  }
+  },
 });
