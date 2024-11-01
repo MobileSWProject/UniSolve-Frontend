bfd8a343d702360cd15754c4ae6e0f1136ae1daf
ICE0208 <doky3307@naver.com>
2024-11-01 23:31:05 +0900

    dev: 경고 표시 표시안되도록 설정

--- unisolve/app/_layout.jsx
+++ unisolve/app/_layout.jsx
@@ -1,3 +1,15 @@
+// react-native-syntax-highlighter의 defaultProps 경고 메세지 앱에서 안나오게
+import { LogBox } from "react-native";
+LogBox.ignoreLogs(["defaultProps"]);
+// react-native-syntax-highlighter의 defaultProps 에러 메세지 로그에서 안나오게
+const originalError = console.error;
+console.error = (message, ...args) => {
+  if (typeof message === "string" && message.includes("defaultProps")) {
+    return;
+  }
+  originalError(message, ...args);
+};
+
 import { Stack } from "expo-router";
 
 export default function Layout() {
