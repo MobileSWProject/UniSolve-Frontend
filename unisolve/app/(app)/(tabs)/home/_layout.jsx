80098e4d46781513f91ba6464a0fd7f97dce8dca
ICE0208 <doky3307@naver.com>
2024-11-11 01:45:11 +0900

    remove: 사용하지 않는 페이지 주석처리

--- unisolve/app/(app)/(tabs)/home/_layout.jsx
+++ unisolve/app/(app)/(tabs)/home/_layout.jsx
@@ -4,20 +4,21 @@ export default function HomeLayout() {
   return (
     <Stack
       initialRouteName="index"
-      screenOptions={{ headerShown: false }}>
+      screenOptions={{ headerShown: false }}
+    >
       <Stack.Screen
         name="index"
         options={{ title: "Home" }}
       />
       {/* 질문하기 페이지 밑에 추가 */}
-      <Stack.Screen
+      {/* <Stack.Screen
         name="logopage"
         options={{ title: "질문하기 페이지( 여기에 질문하기 추가)" }}
-      />
-      <Stack.Screen
+      /> */}
+      {/* <Stack.Screen
         name="subPage"
         options={{ title: "Home 서브 페이지" }}
-      />
+      /> */}
     </Stack>
   );
 }
