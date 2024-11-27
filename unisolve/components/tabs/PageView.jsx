ec21907a72364d1700d207d573df846a3c372f45
ICE0208 <doky3307@naver.com>
2024-11-28 02:10:37 +0900

    style: 상태바 뒷배경

--- unisolve/components/tabs/PageView.jsx
+++ unisolve/components/tabs/PageView.jsx
@@ -3,7 +3,7 @@ import Community from "./community/Community";
 import Me from "./me/Me";
 import { useState } from "react";
 import { getStatusBarHeight } from "react-native-status-bar-height";
-import { SafeAreaView, StatusBar } from "react-native";
+import { SafeAreaView, StatusBar, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { styles } from "../../styles/tabs/home/HomeStyle";
 import { mainColor } from "../../constants/Colors";
@@ -15,20 +15,22 @@ export default function PageView({ type }) {
   const [modalType, setModalType] = useState("");
 
   return (
-    <SafeAreaView style={[styles.container, { marginTop: statusBarSize }]}>
-      <StatusBar backgroundColor={mainColor} barStyle="white-content" />
-      {
-        type === "home" ?
-        <Home type={modalType} setType={setModalType} visible={modalVisible} setVisible={setModalVisible} /> :
-        type === "community" ?
-        <GestureHandlerRootView>
-          <Community type={modalType} setType={setModalType} visible={modalVisible} setVisible={setModalVisible} />
-        </GestureHandlerRootView> :
-        type === "me" ?
-        <Me type={modalType} setType={setModalType} visible={modalVisible} setVisible={setModalVisible} /> :
-        null
-      }
-      <ModalView type={modalType} visible={modalVisible} setVisible={setModalVisible} />
-    </SafeAreaView>
+    <View style={{flex: 1, backgroundColor: mainColor}}>
+      <SafeAreaView style={[styles.container, { marginTop: statusBarSize}]}>
+        <StatusBar backgroundColor={mainColor} barStyle="white-content" />
+        {
+          type === "home" ?
+          <Home type={modalType} setType={setModalType} visible={modalVisible} setVisible={setModalVisible} /> :
+          type === "community" ?
+          <GestureHandlerRootView>
+            <Community type={modalType} setType={setModalType} visible={modalVisible} setVisible={setModalVisible} />
+          </GestureHandlerRootView> :
+          type === "me" ?
+          <Me type={modalType} setType={setModalType} visible={modalVisible} setVisible={setModalVisible} /> :
+          null
+        }
+        <ModalView type={modalType} visible={modalVisible} setVisible={setModalVisible} />
+      </SafeAreaView>
+    </View>
   );
 }
