4e59a4b995a0fecf529629f7636437b9dbd6bba0
ICE0208 <doky3307@naver.com>
2024-12-02 23:02:49 +0900

    fix: 중첩 문제 콘솔에서도 간단하게 뜨게함

--- unisolve/app/_layout.jsx
+++ unisolve/app/_layout.jsx
@@ -3,7 +3,15 @@ import { LogBox } from "react-native"; // react-native-syntax-highlighter의 def
 LogBox.ignoreLogs(["defaultProps"]);
 const originalError = console.error; // react-native-syntax-highlighter의 defaultProps 에러 메세지 로그에서 안나오게
 console.error = (message, ...args) => {
-  if (typeof message === "string" && message.includes("defaultProps")) return;
+  if (typeof message === "string" && message.includes("defaultProps")) {
+    console.log("defaultProps 어쩌구 오류");
+    return;
+  }
+
+  if (message.includes("VirtualizedLists should never be nested")) {
+    console.log("VirtualizedLists 어쩌구 오류");
+    return; // 경고를 무시
+  }
   originalError(message, ...args);
 };
 
